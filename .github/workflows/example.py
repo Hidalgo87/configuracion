0ff6f4c68272751ff0045cc1f4bfd1f1a5c4e69e
Hidalgo87 <110195163+Hidalgo87@users.noreply.github.com>
2024-09-06 17:14:55 -0500

    Update example.py

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -1,17 +1,20 @@
 import schedule
 import time
 
+# Variable de estado para terminar el bucle después de ejecutar la tarea
+tarea_ejecutada = False
+
 def tarea_de_prueba():
+    global tarea_ejecutada
     # Imprime un mensaje para probar la programación
     print('¡La tarea programada se ha ejecutado con éxito!')
-
-# Programa la tarea de prueba a una hora específica
-hora_envio = '17:08'  # Hora en formato 24 horas
+    # Marca la tarea como ejecutada para detener el bucle
+    tarea_ejecutada = True
 
 # Programa la tarea
-schedule.every().day.at(hora_envio).do(tarea_de_prueba)
+schedule.every(1).minutes.do(tarea_de_prueba)
 
 # Ejecuta el programador
-while True:
+while not tarea_ejecutada:
     schedule.run_pending()
-    time.sleep(1)  # Espera un minuto antes de volver a comprobar el horario
+    time.sleep(3)  # Espera un segundo antes de volver a comprobar el horario
