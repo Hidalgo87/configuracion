601dc25f58eec06bb4a74a3dabd689dbe4f03b81
Hidalgo87 <110195163+Hidalgo87@users.noreply.github.com>
2024-09-06 17:03:54 -0500

    Update example.py

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -1,4 +1,17 @@
-print("Hola mundo")
+import schedule
+import time
 
-for i in range(10):
-    print('witsoonn' + 'n'*i)
+def tarea_de_prueba():
+    # Imprime un mensaje para probar la programación
+    print('¡La tarea programada se ha ejecutado con éxito!')
+
+# Programa la tarea de prueba a una hora específica
+hora_envio = '17:04'  # Hora en formato 24 horas
+
+# Programa la tarea
+schedule.every().day.at(hora_envio).do(tarea_de_prueba)
+
+# Ejecuta el programador
+while True:
+    schedule.run_pending()
+    time.sleep(1)  # Espera un minuto antes de volver a comprobar el horario
